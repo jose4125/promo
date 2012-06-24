58d82b8f22e8be97317fc2a3042e466f23f482ee
jose david lombana <jose4125@gmail.com>
2012-06-24 14:11:54 -0500

    commit 5 archivos

--- configuracion.php
+++ configuracion.php
@@ -1,9 +1,9 @@
 <?php
-define('IPSERVIDORBD', '127.0.0.1');
+define('IPSERVIDORBD', '199.188.205.16');
 define('PUERTO', '3306');
-define('USUARIOBD', 'root');
-define('CLAVEBD', '');
-define('BD', 'usuariosalkosto');
+define('USUARIOBD', 'ruursw_promo');
+define('CLAVEBD', 'qwerty123');
+define('BD', 'ruursw_usuariosalkosto');
 define('DRIVER', 'mysql.php');
 require_once(DRIVER);
 ?>
